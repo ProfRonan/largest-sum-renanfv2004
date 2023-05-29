4b2d8c825f4885e8cfb3017b596165c6825b4904
renanfv2004 <128155898+renanfv2004@users.noreply.github.com>
2023-05-28 21:57:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 """Module with functions."""
 
-
 def largest_sum(numbers: list[int]) -> tuple[int, int]:
     """Encontra e retorna os dois números que somados dão o maior valor."""
-    primeiro = 0  # o primeiro número da soma
-    segundo = 0  # o segundo número da soma
-    return primeiro, segundo
+    if len(numbers) < 2:
+        return None
+    else:
+        primeiro = numbers[0]  # o primeiro número da soma
+        for i in range(0, len(numbers)):
+            if numbers[i] > primeiro:
+                primeiro = numbers[i]
+        numbers.remove(primeiro)
+        segundo = numbers[0]
+        for j in range(0, len(numbers)):
+            if numbers[j] > segundo:
+                segundo = numbers[j]         
+        return segundo, primeiro
